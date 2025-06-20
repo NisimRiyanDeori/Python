485856fd7fc556d2ed436a2a34521288149aa2aa
NisimRiyanDeori <nisimriyandeori@gmail.com>
2025-06-20 19:55:10 +0530

    Add files via upload

---
+++ DATA Types.py
@@ -0,0 +1,13 @@
+print("Data types in python")
+a="Nisim"
+b=15
+c=12.5
+d=True
+print("Name of student",a)
+print("data type of variable is",type(a))
+print("value of variable b is",type(b))
+print("data type of variable is",type(b))
+print("value of variable c is",type(c))
+print("data type of variable is",type(c))
+print("value in d is",type(d))
+print("data type of d is",type(d))
