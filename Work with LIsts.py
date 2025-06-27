4d7e1bc7026649e25e62c9783467fb9a6e52992a
NisimRiyanDeori <nisimriyandeori@gmail.com>
2025-06-27 19:55:35 +0530

    Add files via upload

---
+++ Work with LIsts.py
@@ -0,0 +1,11 @@
+lst = ['Apple', 'Guava', 'Mango', 'Banana', 'Kiwi']
+
+print("Length of list:", len(lst))
+print("First element", lst[0])
+print("Last element", lst[-1])
+
+lst.append('Papaya')
+print("Updated List :", lst)
+
+lst.remove('Guava')
+print("Updated list")
